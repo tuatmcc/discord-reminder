6ed945309dd6a3cb69413c0c819c4fc71c214310
Ryohei Kobayashi <kobaryo222912@gmail.com>
2024-04-19 22:51:25 +0900

    channel, role の id の型の変更

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,26 +1,67 @@
 import { DrizzleD1Database, drizzle } from 'drizzle-orm/d1';
-import { events } from './schema';
+import { events, users, roles, mention_roles, mention_users } from './schema';
 import { eq } from 'drizzle-orm';
+import { Event, FullEvent } from '../types/event';
+import { NotifyType } from '../types/notifyType';
+import { formatDateToString, parseStringToDate } from './date';
+
+type DBEvent = {
+    id: number;
+    title: string;
+    content: string;
+    date: string;
+    notify_type: string;
+    channel_id: string;
+};
+
+const castEventFromDBToFullEvent = (dbEvents: DBEvent): FullEvent => {
+    const parsedDate = parseStringToDate(dbEvents.date);
+    const date = parsedDate.success ? parsedDate.date : new Date();
+    const notifyType = dbEvents.notify_type == 'once' ? ('once' as NotifyType) : ('normal' as NotifyType);
+    return {
+        id: dbEvents.id,
+        title: dbEvents.title,
+        content: dbEvents.content,
+        date: date,
+        notify_type: notifyType,
+        channel_id: dbEvents.channel_id,
+    };
+};
 
 export class dbUtil {
     db: DrizzleD1Database;
     constructor(db: D1Database) {
         this.db = drizzle(db);
     }
-    async createEvent(name: string, date: string) {
-        return this.db.insert(events).values({ name: name, date: date }).run();
+    async createEvent(event: Event, channelId: string, notifyType: NotifyType = 'normal' as NotifyType) {
+        return this.db
+            .insert(events)
+            .values({
+                title: event.title,
+                content: event.content,
+                date: formatDateToString(event.date),
+                notify_type: notifyType,
+                channel_id: channelId,
+            })
+            .run();
     }
     async readEvents() {
-        return this.db.select().from(events).all();
+        return (await this.db.select().from(events).all()).map((event) => castEventFromDBToFullEvent(event));
     }
     async deleteEvent(id: number) {
         const deletedEvent = (await this.db.delete(events).where(eq(events.id, id)).returning())[0];
         return deletedEvent;
     }
+    async checkUserExists(id: string) {
+        return (await this.db.select().from(users).where(eq(users.id, id)).all()).length > 0;
+    }
+    async checkRoleExists(id: string) {
+        return (await this.db.select().from(roles).where(eq(roles.id, id)).all()).length > 0;
+    }
     async checkEventExists(id: number) {
         return (await this.db.select().from(events).where(eq(events.id, id)).all()).length > 0;
     }
-    async checkEventExistsByName(name: string) {
-        return (await this.db.select().from(events).where(eq(events.name, name)).all()).length > 0;
+    async checkEventExistsByTitle(name: string) {
+        return (await this.db.select().from(events).where(eq(events.title, name)).all()).length > 0;
     }
 }
--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -6,30 +6,40 @@ export const events = sqliteTable('events', {
     content: text('content').notNull(),
     date: text('date').notNull(),
     notify_type: text('notify_type').notNull(),
-    channel_id: text('channel_id').notNull().references(() => channels.id),
+    channel_id: text('channel_id')
+        .notNull()
+        .references(() => channels.id),
 });
 
 export const mention_users = sqliteTable('mention_users', {
-    event_id: text('event_id').primaryKey().references(() => events.id),
-    user_id: text('user_id').notNull().references(() => users.id),
+    event_id: text('event_id')
+        .primaryKey()
+        .references(() => events.id),
+    user_id: text('user_id')
+        .notNull()
+        .references(() => users.id),
 });
 
 export const mention_roles = sqliteTable('mention_roles', {
-    event_id: text('event_id').primaryKey().references(() => events.id),
-    role_id: text('role_id').notNull().references(() => roles.id),
+    event_id: text('event_id')
+        .primaryKey()
+        .references(() => events.id),
+    role_id: text('role_id')
+        .notNull()
+        .references(() => roles.id),
 });
 
 export const users = sqliteTable('users', {
     id: text('id').primaryKey(),
-    name : text('name').notNull(),
+    name: text('name').notNull(),
 });
 
 export const roles = sqliteTable('roles', {
     id: text('id').primaryKey(),
-    name : text('name').notNull(),
+    name: text('name').notNull(),
 });
 
 export const channels = sqliteTable('channels', {
     id: text('id').primaryKey(),
-    name : text('name').notNull(),
+    name: text('name').notNull(),
 });
